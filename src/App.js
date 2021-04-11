092ed43e452da02cf5fed2110f255ee8f8b85a16
kamrul hasan <kamrulhasan62579@gmail.com>
2021-04-10 21:45:17 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <Users></Users>
       </header>
     </div>
   );
 }
+function Users(){
+  const [users, setUsers] = useState([])
+  useEffect(() =>{
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return(
+    <div>
+      {
+        users.map( user =>  <SinglePosts usr={user}></SinglePosts> ) 
+      }
+    </div>
+  )
+}
+function SinglePosts(props) {
+  const singleUserStyle ={
+    height: '250px',
+    width: '600px',
+    border: '3px solid yellow',
+    borderRadius: '10px',
+    margin: '25px',
+    padding: '5px',
+    backgroundColor: 'orange'
+  }
+  const {title, body} = props.usr;
+  return(
+    <div style={singleUserStyle}>
+     
+      <h3 style={{color: 'red'}}>Title: {title}</h3>
+      <h4 style={{color: 'blue'}}>Body: {body}</h4>
+    </div>
+  )
+}
+
 
 export default App;
